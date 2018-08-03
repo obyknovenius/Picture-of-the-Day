4a4f6c38b43d571905ef2d70702f1ee1b8283255
Vitaly Dyachov <obyknovenius@me.com>
2018-08-03 11:26:29 +0200

    Initial Commit

---
+++ Picture of the Day/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Picture of the Day
+//
+//  Created by Vitaly Dyachkov on 8/3/18.
+//  Copyright © 2018 Vitaly Dyachkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
